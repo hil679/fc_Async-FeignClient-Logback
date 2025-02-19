a3bc880be00933c32b23cb9498b9d719b5a253c2
hil679 <hil679922@gmail.com>
2025-02-19 12:53:57 +0900

    Add [FeignClient]: commit init feign client config

---
+++ fiegn/src/main/java/dev/be/fiegn/FiegnApplication.java
@@ -0,0 +1,13 @@
+package dev.be.fiegn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiegnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiegnApplication.class, args);
+    }
+
+}
---
+++ fiegn/src/test/java/dev/be/fiegn/FiegnApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.be.fiegn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiegnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
