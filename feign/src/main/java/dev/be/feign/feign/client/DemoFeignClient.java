11b242a820d75fe4fe100fa7478b8ffd6c0b200b
hil679 <hil679922@gmail.com>
2025-02-20 14:34:20 +0900

    Add [FeignClient]: test example of feign client

---
+++ feign/src/main/java/dev/be/feign/feign/client/DemoFeignClient.java
@@ -0,0 +1,21 @@
+package dev.be.feign.feign.client;
+
+import dev.be.feign.common.dto.BaseResponseInfo;
+import dev.be.feign.feign.config.DemoFeignConfig;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(
+        name = "demo-client", // like PK
+        url = "${feign.url.prefix}", // target server url
+        configuration = DemoFeignConfig.class
+)
+public interface DemoFeignClient {
+    @GetMapping("/get") // -> http://localhost:8080/target_server/get
+    ResponseEntity<BaseResponseInfo> callGet(@RequestHeader("CustomHeaderName") String customHeader,
+                                             @RequestParam("name") String name,
+                                             @RequestParam("age") Long age);
+}
--- feign/src/main/java/dev/be/feign/service/DemoService.java
+++ feign/src/main/java/dev/be/feign/service/DemoService.java
@@ -1,10 +1,18 @@
 package dev.be.feign.service;
 
+import dev.be.feign.common.dto.BaseResponseInfo;
+import dev.be.feign.feign.client.DemoFeignClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class DemoService {
+    private final DemoFeignClient demoFeignClient;
     public String get() {
+        ResponseEntity<BaseResponseInfo> response = demoFeignClient.callGet("CustomHeader", "CustomNm", 1L);
+        System.out.println("Name : " + response.getBody().getName());
         return "get";
     }
 }
